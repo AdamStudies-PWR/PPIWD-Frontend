e495e87915bf787a977815f4e8bbc82262c97602
KalinaMichal <michal230915@gmail.com>
2023-06-03 18:55:02 +0200

    dodalem liste urzadzen i cala funkjonalnosc bluetooth

--- app/src/main/java/com/pwr/activitytracker/data/model/ui/train/login/LoginActivity.java
+++ app/src/main/java/com/pwr/activitytracker/data/model/ui/train/login/LoginActivity.java
@@ -1,23 +1,42 @@
 package com.pwr.activitytracker.data.model.ui.train.login;
 
+import static com.pwr.activitytracker.BluetoothActivity.reconnect;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.UnsupportedModuleException;
+import com.mbientlab.metawear.android.BtleService;
 import com.pwr.activitytracker.BluetoothActivity;
 import com.pwr.activitytracker.MainActivity;
 import com.pwr.activitytracker.R;
@@ -28,11 +47,19 @@ import com.pwr.activitytracker.network.PostAsyncTask;
 import com.pwr.activitytracker.network.models.LoginCredentials;
 import com.pwr.activitytracker.network.models.LoginUserData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import bolts.Task;
 import es.dmoral.toasty.Toasty;
+import com.mbientlab.metawear.module.Settings;
+import com.pwr.activitytracker.sensors.DeviceController;
+import android.Manifest;
 
-public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
+public class LoginActivity extends AppCompatActivity implements ServiceConnection, AsyncCallBack {
 
     private final String PREFERENCES_KEY = "user-prefs-key";
     private LoginViewModel loginViewModel;
@@ -41,11 +68,66 @@ public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
     private String IP = "10.0.2.2";
     private String PORT = "5242";
 
+    private Set<BluetoothDevice> pairedDevices = null;
+
+    private BluetoothDevice selectedDevice;
+
+    private boolean deviceConnected = false;
+
+    private BtleService.LocalBinder serviceBinder;
+
+    private MetaWearBoard sensorBoard;
+
+    private static DeviceController deviceController;
+
+    public static Task<Void> reconnect(final MetaWearBoard board)
+    {
+        return board.connectAsync()
+                .continueWithTask(task -> {
+                    if (task.isFaulted())
+                    {
+                        return reconnect(board);
+                    } else if (task.isCancelled())
+                    {
+                        return task;
+                    }
+                    return Task.forResult(null);
+                });
+    }
+
+    void setConnInterval(Settings settings)
+    {
+        if (settings != null)
+        {
+            Settings.BleConnectionParametersEditor editor = settings.editBleConnParams();
+            if (editor != null)
+            {
+                editor.maxConnectionInterval(11.25f).commit();
+            }
+        }
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder service)
+    {
+        serviceBinder = (BtleService.LocalBinder) service;
+        //connectToSensor(); TODO: Panie Adamie, mogę to usunąć? @mkalina
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        deviceController = new DeviceController();
         super.onCreate(savedInstanceState);
+        getApplicationContext().bindService(new Intent(this, BtleService.class),
+                this, BIND_AUTO_CREATE);
 
+        //tloadUserPrefs();
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         loadServerSettings();
@@ -62,7 +144,7 @@ public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
             if (loginFormState == null) {
                 return;
             }
-            loginButton.setEnabled(loginFormState.isDataValid());
+            loginButton.setEnabled(loginFormState.isDataValid() && deviceConnected);
             registerButton.setEnabled(loginFormState.isDataValid());
             if (loginFormState.getUsernameError() != null) {
                 usernameEditText.setError(getString(loginFormState.getUsernameError()));
@@ -148,8 +230,49 @@ public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
 
         ipText.setText(IP);
         portText.setText(PORT);
+
+
+
+
+        ListView listView = findViewById(R.id.listview);
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        try {
+            pairedDevices = bluetoothAdapter.getBondedDevices();
+        } catch (SecurityException exception) {
+            Log.e("BLUETOOTH", "Error granting permission: " + exception.toString());
+            Toast.makeText(getApplicationContext(), R.string.permissionError, Toast.LENGTH_SHORT).show();
+        }
+        List<String> devicesNames = new ArrayList<>();
+        if(pairedDevices !=null){
+            devicesNames = pairedDevices.stream().map(BluetoothDevice::getName).collect(Collectors.toList());
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, devicesNames);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (!arePermissionsGranted()) {
+                    requestBluetoothPermission();
+                }
+                 selectedDevice = nthElement(pairedDevices, position);
+                 connectToSensor();
+            }
+        });
     }
 
+    public static final <T> T nthElement(Iterable<T> data, int n){
+        int index = 0;
+        for(T element : data){
+            if(index == n){
+                return element;
+            }
+            index++;
+        }
+        return null;
+    }
     private void saveServerSettings()
     {
         SharedPreferences settings = getApplicationContext().getSharedPreferences(PREFERENCES_KEY, 0);
@@ -165,6 +288,56 @@ public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
         editor.putString("PORT", PORT);
 
         editor.apply();
+    }
+
+    private void connectToSensor()
+    {
+        if(selectedDevice!=null) {
+            connectToDevice(selectedDevice);
+        }
+        else {
+            Log.e("BLUETOOTH", "Error, device is null");
+            Toast.makeText(getApplicationContext(), R.string.permissionError, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void connectToDevice(BluetoothDevice device)
+    {
+        serviceBinder.removeMetaWearBoard(device);//tutaj jest blad
+        sensorBoard = serviceBinder.getMetaWearBoard(device);
+
+        sensorBoard.connectAsync().continueWithTask(task -> {
+            if (task.isCancelled()) return task;
+            return task.isFaulted() ? reconnect(sensorBoard) : Task.forResult(null);
+        }).continueWith(task -> {
+            if (!task.isCancelled())
+            {
+                setConnInterval(sensorBoard.getModule(Settings.class));
+                try
+                {
+                    deviceController.setSensors(sensorBoard);
+                    deviceConnected = true;
+                }
+                catch (UnsupportedModuleException exception)
+                {
+                    runOnUiThread(() -> Toast.makeText(getApplicationContext(), R.string.connectedFailure,
+                            Toast.LENGTH_SHORT).show());
+                }
+                runOnUiThread(() -> {
+                    Toast.makeText(getApplicationContext(), R.string.connectedInfo,
+                            Toast.LENGTH_SHORT).show();
+                    //To zakomentowane jest z data gettera. Zostawiam, moze sie tu potem przyda
+                    //TextView text = findViewById(R.id.deviceInfoText);
+                    //text.setText(R.string.deviceStatusConnected);
+                    //ImageView image = findViewById(R.id.deviceInfoImage);
+                    //image.setImageResource(R.drawable.baseline_bluetooth_connected_24);
+                });
+                ListView listView = findViewById(R.id.listview);
+                listView.setVisibility(View.GONE);//to nie dziala chyba dlatego ze jest tu linear view.
+                //Ale w sumie to nie musi dzialac, moze poprawie gui aby to sie jakos fajnie prezentowalo
+            }
+            return null;
+        });
     }
 
     private void auto_login() {
@@ -249,5 +422,33 @@ public class LoginActivity extends AppCompatActivity implements AsyncCallBack {
             }
         }
     }
+    private void requestBluetoothPermission()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        {
+            this.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
+        }
+        else
+        {
+            this.requestPermissions(new String[]{Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+    private boolean arePermissionsGranted()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        {
+            return ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
+                    == PackageManager.PERMISSION_GRANTED;
+        }
+        else
+        {
+            return ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        }
+    }
 }
-
